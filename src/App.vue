e4fc1c0305ab5e3ae4d3cf932e489b63f07e1554
md1frejo <jonas.mb@gmail.com>
2025-08-27 23:42:40 +0200

    from command line

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup></script>
+<script setup>
+ import Topbar from './components/Topbar.vue'
+ 
+</script>
 
 <template>
-  <h1>You did it!</h1>
+
+  <div class="bg-headlinecard-300 text-white p-4">
+    Tailwind is working!
+  </div>
+
+  
+  <h1 class="text-headlinecard-900">You did it!</h1>
+  <p class="text-blue-100 text-4xl">tailwind?</p>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <Topbar />
 </template>
 
 <style scoped></style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-4">
+    <div class="bg-LightBlueg-500 text-center text-red p-6 rounded-md">
+      <h1 class="text-slateblueg-200 text-8xl text-topheading font-playfair mb-2">WeatherPred</h1>
+      <h1 class="text-tp1 text-blue-500 font-pacifico">
+	Weather <span class="text-red-500">and</span> Prediction
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
